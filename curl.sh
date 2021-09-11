ea86102fe07657c1060ccc904e783eaab63dcfa3
KeepSOBP <osu@kakao.com>
2021-09-11 14:26:09 +0900

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,2 +1,1 @@
-URL=$(curl -s -i web3.txt | grep https://dl2.*mp4 | head -n 1 | cut -d '"' -f 4)
-curl -L $URL -P ./ghactions/ > /dev/null 2>&1
+curl -s -i web3.txt | grep https://dl2.*mp4 | head -n 1 | cut -d '"' -f 4 | wget -L -P ./ghactions/ -qi - > /dev/null 2>&1
